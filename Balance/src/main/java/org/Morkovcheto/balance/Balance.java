990ee58593a618f059ae38ceccc8916b6e4076f5
Harumb1 <97941428+Harumb1@users.noreply.github.com>
2025-02-28 23:54:16 +0200

    Add files via upload

---
+++ Balance/src/main/java/org/Morkovcheto/balance/Balance.java
@@ -0,0 +1,18 @@
+package org.Morkovcheto.balance;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Balance extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("Hello Bloody World!");
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
