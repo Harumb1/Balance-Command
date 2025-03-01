9a4cbaf8f27a4d499c4cc0bbf3542a4abef09e53
Harumb1 <97941428+Harumb1@users.noreply.github.com>
2025-03-01 22:54:18 +0200

    Update BalanceCommand.java

--- Balance/src/main/java/org/Morkovcheto/balance/Balance.java
+++ Balance/src/main/java/org/Morkovcheto/balance/Balance.java
@@ -1,11 +1,17 @@
 package org.Morkovcheto.balance;
-
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Balance extends JavaPlugin {
+
+public final class Balance extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
+        this.getCommand("balance").setExecutor(new BalanceCommand());
+        getServer().getPluginManager().registerEvents(this, this);
         // Plugin startup logic
 
     }
@@ -15,8 +21,20 @@ public final class Balance extends JavaPlugin {
         // Plugin shutdown logic
     }
 
-    public static JavaPlugin getInstance() {
-        return getPlugin(Balance.class);
+    @EventHandler
+    public void onPlayerBreakBlockEvent(BlockBreakEvent e) {
+        balance++;
+    }
+
+    @EventHandler
+    public void onPlayerPlaceBlockEvent(BlockPlaceEvent e) {
+        balance++;
+    }
+
+    static int balance = 0;
+
+    static int getBlocksBroken() {
+        return balance;
     }
 
 }
