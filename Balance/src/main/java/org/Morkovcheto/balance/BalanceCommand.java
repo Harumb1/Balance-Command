f882f5a468204fa013724ead2bd06fbdd63dca38
Harumb1 <miroslav.mihaylov.pcj@gmail.com>
2025-03-03 19:21:49 +0200

    Update BalanceCommand.java

--- Balance/src/main/java/org/Morkovcheto/balance/BalanceCommand.java
+++ Balance/src/main/java/org/Morkovcheto/balance/BalanceCommand.java
@@ -1,5 +1,4 @@
 package org.Morkovcheto.balance;
-
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -7,29 +6,22 @@ import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-
-import javax.sql.rowset.spi.SyncFactoryException;
 import java.util.List;
 
-import static javax.sql.rowset.spi.SyncFactory.getLogger;
-
-public class BalanceCommand implements CommandExecutor, TabExecutor {
+public class BalanceCommand implements CommandExecutor, TabExecutor{
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String @NotNull [] strings) {
-        if(!(commandSender instanceof Player)){
+        if (!(commandSender instanceof Player)) {
             commandSender.sendMessage("Only players can use this command!");
+            return true;
         }
-        int balance = 0;
-        try {
-            getLogger().info(String.valueOf(balance));
-        } catch (SyncFactoryException e) {
-            throw new RuntimeException(e);
-        }
-        return false;
+        commandSender.sendMessage(String.valueOf(Balance.balance));
+        return true;
     }
 
     @Override
     public @Nullable List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String @NotNull [] strings) {
         return List.of();
     }
+
 }
